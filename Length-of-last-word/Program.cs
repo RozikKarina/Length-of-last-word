c0777ea281e42dbbf6da19803908bc8c59b72449
Karina Rozenboym <karinarozik1@gmail.com>
2024-10-27 04:29:38 +0200

    Добавьте файлы проекта.

---
+++ Length-of-last-word/Program.cs
@@ -0,0 +1,23 @@
+﻿public class Solution
+{
+    public int LengthOfLastWord(string s)
+    {
+        int length = 0;
+        int i = s.Length - 1;
+
+        // Пропускаємо пробіли з кінця рядка
+        while (i >= 0 && s[i] == ' ')
+        {
+            i--;
+        }
+
+        // Підраховуємо довжину останнього слова
+        while (i >= 0 && s[i] != ' ')
+        {
+            length++;
+            i--;
+        }
+
+        return length;
+    }
+}
